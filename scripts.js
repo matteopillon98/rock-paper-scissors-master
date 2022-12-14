6244ad0fcc051e33857fa30831a14a03f0a9ec80
Matteo Pillon <matteopillon98@gmail.com>
2022-12-14 17:48:36 +0100

    Add second frame to game

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ const winning_choices = {
 
 function gameMatch (userChoice){
     let computerChoice = winning_choices[Object.keys(winning_choices)[Math.floor(Math.random()*Object.keys(winning_choices).length)]];
-
+    showGameChoice(userChoice)
     console.log("----------------------------------------------------------------------")
     console.log("COMPUTER CHOOSE: "+computerChoice);
 
@@ -28,4 +28,27 @@ function gameMatch (userChoice){
     document.getElementById('scoreLabel').innerHTML= user_score;
     console.log("PLAYER: "+user_score+" - "+"COMPUTER: "+computer_score)
     console.log("----------------------------------------------------------------------")
+}
+
+function showGameChoice(choice){
+
+    document.getElementById('game').style.display="none";
+    document.getElementById('gameChoice').style.display="flex";
+    var element = document.createElement("button");
+    element.className = "btn_Game";
+
+    if(choice == 'rock'){
+        element.id = "btn_Rock";
+    }
+    if(choice == 'paper'){
+        element.id = "btn_Paper";
+    }
+    if(choice == 'scissors'){
+        element.id = "btn_Scissors";
+    }
+    element.style.setProperty("animation", "pulse-animation 3s infinite");
+    var foo = document.getElementById("gameChoice");
+    //Append the element in page (in span).  
+    foo.appendChild(element)
+
 }
