172cf089e3ae44cf6365a2aa1c9ae278f4149fc6
Matteo Pillon <matteopillon98@gmail.com>
2022-12-15 10:23:28 +0100

    Add result windows and rematch option

--- scripts.js
+++ scripts.js
@@ -2,53 +2,62 @@
 let [computer_score,user_score]=[0,0];
 
 const winning_choices = {
-    rock: 'scissor',
+    rock: 'scissors',
     paper: 'rock',
-    scissor: 'paper',
+    scissors: 'paper',
   };
 
 function gameMatch (userChoice){
     let computerChoice = winning_choices[Object.keys(winning_choices)[Math.floor(Math.random()*Object.keys(winning_choices).length)]];
-    showGameChoice(userChoice)
-    console.log("----------------------------------------------------------------------")
-    console.log("COMPUTER CHOOSE: "+computerChoice);
+    document.getElementById('game').style.display= "none";
+    document.getElementById('gameChoice').style.display= "flex";
+
+    let playerDiv = document.getElementById('playerChoice');
+    let computerDiv = document.getElementById('computerChoice');
+
+    appendElement(playerDiv, userChoice,"YOU CHOOSE");
+    appendElement(computerDiv, computerChoice, "THE HOUSE PICKED");
 
     if(userChoice === computerChoice){
-        alert("DRAW - "+"IL COMPUTER HA SCELTO: "+computerChoice);
+        document.getElementById('matchResult').textContent="DRAW";
     } 
     else if(Object.entries(winning_choices).find(([key, value]) => key === userChoice && value === computerChoice)){
-        alert("USER WIN - "+"IL COMPUTER HA SCELTO: "+computerChoice);
+        document.getElementById('matchResult').textContent="YOU WIN";
         user_score++;
     }
     else{
-        alert("COMPUTER WIN - "+"IL COMPUTER HA SCELTO: "+computerChoice);
+        document.getElementById('matchResult').textContent="YOU LOSE";
         user_score--;
     }
 
     document.getElementById('scoreLabel').innerHTML= user_score;
-    console.log("PLAYER: "+user_score+" - "+"COMPUTER: "+computer_score)
-    console.log("----------------------------------------------------------------------")
 }
 
-function showGameChoice(choice){
+function rematch(){
+    document.getElementById('game').style.display= "flex";
+    document.getElementById('gameChoice').style.display= "none";
+    document.getElementById('playerChoice').innerHTML = "";
+    document.getElementById('computerChoice').innerHTML = "";
+}
 
-    document.getElementById('game').style.display="none";
-    document.getElementById('gameChoice').style.display="flex";
-    var element = document.createElement("button");
-    element.className = "btn_Game";
+function appendElement(div, typeButton, titleText){
+    let button = document.createElement("button");
+    let title = document.createElement("h3");
+    title.appendChild(document.createTextNode(titleText));
 
-    if(choice == 'rock'){
-        element.id = "btn_Rock";
+    button.setAttribute("class","btn_Game");
+    if(typeButton == 'rock'){
+        button.setAttribute("id","btn_Rock");
     }
-    if(choice == 'paper'){
-        element.id = "btn_Paper";
-    }
-    if(choice == 'scissors'){
-        element.id = "btn_Scissors";
-    }
-    element.style.setProperty("animation", "pulse-animation 3s infinite");
-    var foo = document.getElementById("gameChoice");
-    //Append the element in page (in span).  
-    foo.appendChild(element)
 
+    else if(typeButton == 'scissors'){
+        button.setAttribute("id","btn_Scissors");
+    }
+
+    else if(typeButton == 'paper'){
+        button.setAttribute("id","btn_Paper");
+    }
+
+    div.appendChild(title)
+    div.appendChild(button);
 }
